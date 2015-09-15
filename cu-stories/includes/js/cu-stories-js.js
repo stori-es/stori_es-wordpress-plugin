c464190eeae3d96bd83cf4e6beb41ff790663ba2
Gregory Foster <fostgr@consumer.org>
2015-09-15 15:03:02 -0500

    SWP-43: updating the API Key failed validation message

--- cu-stories/includes/js/cu-stories-js.js
+++ cu-stories/includes/js/cu-stories-js.js
@@ -1,13 +1,13 @@
 function doExport(){
-	jQuery(window).attr("location",php_vars.export_script_url); 
-	return true;	
+	jQuery(window).attr("location",php_vars.export_script_url);
+	return true;
 }
 
 function checkAPIKey(event){
 	var result = true;
 	var key = event.which || event.keyCode;
 	var api_key = event.target.value;
-	
+
 	if (key == jQuery.ui.keyCode.ENTER || event.type == "blur"){
 		if(php_vars.api_key != api_key) {
 	        switch(event.target.name){
@@ -31,29 +31,29 @@ function validateAPIKey(api_key){
 			'action': 'validate_api_key',
 			'api_key': api_key
 		};
-	
+
 
 	jQuery.post(ajax_object.ajax_url, data, function(response) {
 		jQuery('#process_id_usr').css('visibility','hidden');
-		
+
 		if(response == 'SUCCESS'){
 			jQuery('#check_result_usr').html('Verified').css('color', 'green');
 		}else{
-			jQuery('#check_result_usr').html("Doesn't exist").css('color', 'red');
+			jQuery('#check_result_usr').html("Invalid key, please try again").css('color', 'red');
 		}
-		
+
 		jQuery('#check_result_usr').css('visibility', 'visible');
 	});
-	
+
 	return true;
-} 
+}
 
 function checkInput(event){
 	var result = true;
 	var key = event.which || event.keyCode;
-	var arrAllowedChars = [jQuery.ui.keyCode.LEFT, 
-	                       jQuery.ui.keyCode.RIGHT, 
-	                       jQuery.ui.keyCode.HOME, 
+	var arrAllowedChars = [jQuery.ui.keyCode.LEFT,
+	                       jQuery.ui.keyCode.RIGHT,
+	                       jQuery.ui.keyCode.HOME,
 	                       jQuery.ui.keyCode.END,
 	                       jQuery.ui.keyCode.BACKSPACE,
 	                       jQuery.ui.keyCode.DELETE];
@@ -84,22 +84,22 @@ function validateCollection(collection_id){
 			'action': 'validate_collection',
 			'collection_id': collection_id
 		};
-	
+
 
 	jQuery.post(ajax_object.ajax_url, data, function(response) {
 		jQuery('#process_ind_id').css('visibility','hidden');
-		
+
 		if(response == 'SUCCESS'){
 			jQuery('#check_result').html('Verified').css('color', 'green');
 		}else{
 			jQuery('#check_result').html("Doesn't exist").css('color', 'red');
 		}
-		
+
 		jQuery('#check_result').css('visibility', 'visible');
 	});
-	
+
 	return true;
-} 
+}
 
 function test(){
 	var data = {
@@ -114,9 +114,9 @@ function test(){
 }
 
 function actionStartSync(){
-	
+
 	var sync_time = jQuery('#custory_refresh_rate_id').val();
-	
+
 	var data = {
 			'action': 'ctrl_sync',
 			'state': 'start',
